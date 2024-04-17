ea94e1f0c56c7216e4b00bc3e37c56943dd78670
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2024-04-17 17:22:46 -0300

    feature advance
    Avanços em users e games e começo da construção de Admin

--- server/app/model/Games.py
+++ server/app/model/Games.py
@@ -3,7 +3,7 @@ from app.shared.dataBase import db
 class Games(db.Model):
 
     __tablename__ = "games"
- 
+
     id = db.Column(db.Integer ,primary_key=True,autoincrement=True)
     gameName = db.Column(db.String(20), unique=True , nullable=False)
     secondGameName = db.Column(db.String(20), unique=True , nullable=False)
