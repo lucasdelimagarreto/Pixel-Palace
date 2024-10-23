ca3e38d4f72dcea8c92e251a95104ca841eb3d4a
Lucas de Lima Garreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2024-10-23 15:12:03 -0300

    Correção atributos e funções
    alteração do atributo price para float e correção de funções de alteração de atributos de games

--- server/app/shared/baseRepository.py
+++ server/app/shared/baseRepository.py
@@ -2,20 +2,23 @@ from app.shared.dataBase import db
 
 class BaseRepository:
     
-    def __init__(self,model):
+    def __init__(self, model):
         self.model = model 
 
-    def save(self,object):
+    def save(self, object):
         db.session.add(object)
         db.session.commit()
 
-    def update(self,object):
+    def update(self, object):
         object.verified = True
         db.session.commit()
 
-    def delete(self,object):
+    def delete(self, object):
         db.session.delete(object)
         db.session.commit()
         
-    def getAll(self):
-        ...
+    def get_all(self):
+        return db.session.query(self.model).all()  # Retorna todos os objetos do modelo
+
+    def get_by_id(self, id):
+        return db.session.query(self.model).filter_by(id=id).first()  # Busca um objeto pelo ID
